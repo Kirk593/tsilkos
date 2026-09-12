927988e262361a3a1da87ad20d1f8a614e1066d6
Kirk593 <iamkathrina158@gmail.com>
2026-09-13 00:39:29 +0800

    Update ext2.h

--- kernelsource/kernel/fs/ext2.h
+++ kernelsource/kernel/fs/ext2.h
@@ -3,23 +3,35 @@
 
 #include <stdint.h>
 
-typedef struct {
-    uint32_t inode_count;
-    uint32_t block_count;
-    uint32_t reserved_blocks;
-    uint32_t free_blocks;
-    uint32_t free_inodes;
+typedef struct __attribute__((packed)) {
+    uint32_t inodes_count;
+    uint32_t blocks_count;
+    uint32_t reserved_blocks_count;
+    uint32_t free_blocks_count;
+    uint32_t free_inodes_count;
     uint32_t first_data_block;
-    uint32_t block_size;
+    uint32_t log_block_size;
+    uint32_t log_fragment_size;
     uint32_t blocks_per_group;
+    uint32_t fragments_per_group;
     uint32_t inodes_per_group;
-    uint32_t mtime;
-    uint32_t wtime;
-    uint16_t mnt_count;
-    int16_t  max_mnt_count;
-    uint16_t magic;
-} __attribute__((packed)) Ext2Superblock;
+    uint32_t mount_time;
+    uint32_t write_time;
+    uint16_t mount_count;
+    uint16_t max_mount_count;
 
-Ext2Superblock *read_superblock();
+    uint16_t magic;              /* offset 56 = 0x38 */
+    uint16_t state;
+    uint16_t errors;
+    uint16_t minor_revision;
+    uint32_t last_check;
+    uint32_t check_interval;
+    uint32_t creator_os;
+    uint32_t revision_level;
+    uint16_t default_uid;
+    uint16_t default_gid;
+} Ext2Superblock;
+
+Ext2Superblock *read_superblock(void);
 
 #endif
