68745a7646f502607943892a9b4b1b4cfc4348ad
Kirk593 <iamkathrina158@gmail.com>
2026-09-13 00:25:18 +0800

    Update syscalls.c

--- kernelsource/kernel/syscalls.c
+++ kernelsource/kernel/syscalls.c
@@ -1,28 +1,37 @@
-typedef void (*syscall_fn)();
+#include "syscalls.h"
+#include "interrupts.h"
+#include "video/fb.h"
+#include <stdint.h>
 
-void sys_write() {
-    draw_string("output", 100, 100, 0xFFFFFF);
+typedef void (*syscall_fn)(void);
+
+static void sys_exit(void)
+{
+    __asm__ volatile ("cli");
+    for (;;) {
+        __asm__ volatile ("hlt");
+    }
 }
 
-void sys_exit() {
-    __asm__("hlt");
+static void sys_write(void)
+{
+    draw_string("syscall: output", 100, 132, 0xFFFFFFu);
 }
 
-syscall_fn syscall_table[] = {
+static syscall_fn syscall_table[] = {
     sys_exit,
     sys_write,
 };
 
-void syscall_handler() {
-    int syscall_num;
-    __asm__("mov %%eax, %0" : "=r"(syscall_num));
-
-    if (syscall_num < 8) {
+void syscall_dispatch(uint32_t syscall_num)
+{
+    const uint32_t count = (uint32_t)(sizeof(syscall_table) / sizeof(syscall_table[0]));
+    if (syscall_num < count)
         syscall_table[syscall_num]();
-    }
 }
 
-void idt_init() {
-    idt_set_entry(0x80, (uint32_t)syscall_handler);
-    idt_load();
+void syscalls_init(void)
+{
+    /* Present, 32-bit interrupt gate. DPL 0 for now (kernel callers only). */
+    idt_set_entry(0x80u, (uint32_t)(uintptr_t)syscall_interrupt_stub, 0x8Eu);
 }
