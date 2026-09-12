070fade7eef50e3deb78851563f0256fdbee523a
Kirk593 <iamkathrina158@gmail.com>
2026-09-13 00:28:03 +0800

    Update fb.c

--- kernelsource/kernel/video/fb.c
+++ kernelsource/kernel/video/fb.c
@@ -1,35 +1,47 @@
+#include "fb.h"
 #include "font.h"
 
-static unsigned int *fb;
-static int fb_width;
-static int fb_height;
+static uint32_t *fb;
+static uint32_t fb_width;
+static uint32_t fb_height;
+static uint32_t fb_pitch;
 
-void draw_pixel(int x, int y, unsigned int color) {
-    fb[y * fb_width + x] = color;
-}
-
-void fb_init(unsigned int *addr, int width, int height) {
-    fb = addr;
+void fb_init(void *addr, uint32_t width, uint32_t height, uint32_t pitch)
+{
+    fb = (uint32_t *)addr;
     fb_width = width;
     fb_height = height;
+    fb_pitch = pitch;
 }
 
-void draw_char(char c, int x, int y, unsigned int color) {
+void draw_pixel(uint32_t x, uint32_t y, uint32_t color)
+{
+    if (!fb || x >= fb_width || y >= fb_height || fb_pitch < 4)
+        return;
+
+    uint32_t pixels_per_row = fb_pitch / 4;
+    fb[y * pixels_per_row + x] = color;
+}
+
+static void draw_char(char c, uint32_t x, uint32_t y, uint32_t color)
+{
     const unsigned char *glyph = font_8x8[(unsigned char)c];
 
-    for (int row = 0; row < 8; row++) {
-        for (int col = 0; col < 8; col++) {
-            if (glyph[row] & (1 << (7 - col))) {
+    for (uint32_t row = 0; row < 8; row++) {
+        for (uint32_t col = 0; col < 8; col++) {
+            if (glyph[row] & (1u << (7u - col)))
                 draw_pixel(x + col, y + row, color);
-            }
         }
     }
 }
 
-void draw_string(char *str, int x, int y, unsigned int color) {
+void draw_string(const char *str, uint32_t x, uint32_t y, uint32_t color)
+{
+    if (!str)
+        return;
+
     while (*str) {
-        draw_char(*str, x, y, color);
+        draw_char(*str++, x, y, color);
         x += 8;
-        str++;
     }
 }
