1e9c4ad9e3651b2b04c6cea68aa6cf0ee3df6ccf
Kirk593 <iamkathrina158@gmail.com>
2026-09-13 00:41:23 +0800

    Update syscall_stub.s

--- kernelsource/arch/x86/boot/syscall_stub.s
+++ kernelsource/arch/x86/boot/syscall_stub.s
@@ -3,13 +3,14 @@ BITS 32
 section .text
 
 global syscall_interrupt_stub
-extern syscall_handler
+extern syscall_dispatch
 
 syscall_interrupt_stub:
     pushad
 
-    cld
-    call syscall_handler
+    push eax
+    call syscall_dispatch
+    add esp, 4
 
     popad
     iretd
